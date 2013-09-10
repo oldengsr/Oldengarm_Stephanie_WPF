07fe81b0fc2f4e0d0332b189cc32f9c411b384fb
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-10 10:07:25 -0400

    Expressions - Arithmetic Operators Example

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,5 +1,7 @@
-//Expressions - Age Example
+//Expressions - Arithmetic Operators
 
-var yearBorn = 1990;
-var age = 2013 - yearBorn - 1;
-console.log(age);
+//area of the triangle is half the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
