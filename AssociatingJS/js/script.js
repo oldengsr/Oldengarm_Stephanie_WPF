8a238f2b42d521397b0dd27826b66d2e3cf93822
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-10 10:20:39 -0400

    Using Arrays in Expressions Example

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,15 +1,7 @@
-//Expressions - PEMDAS - The Order of Operations
+//Using Arrays in Expressions
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var orangeBins = [234, 567, 883];
 
-//the average is the sum of the items divided by the number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-//console.log(average);
+var total = orangeBins[0] + orangeBins[1] + orangeBins [2];
 
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-console.log(perimeter);
+console.log(total);
