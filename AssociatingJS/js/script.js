c5714ffbab82390f7da8656bff24f811b31dceb4
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-09 21:57:33 -0400

    Defining Variables Example

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,3 +1,8 @@
-// JavaScript
+// Defining Variables
 
-var age;
+var age; //declaration
+age = 22; //definition
+
+// storage container = value
+
+var yearOfBirth = 1990; //declaring and defining on the same line
