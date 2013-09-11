d3cc613f480a0ebd26fe02ac7da123629dcbebe5
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 09:11:10 -0400

    Dog Years Expression Worksheet assignment.

---
+++ Expressions_worksheet/js/DogYears.js
@@ -0,0 +1,11 @@
+//Stephanie Oldengarm, Sept. 11, 2013, Expression Worksheet - Dog Years
+
+//Calculate Sparky's age in dog years.
+
+var SparkyHumanAge = 4;
+
+//For every 1 human year, a dog ages 7 years.
+var DogYears = SparkyHumanAge * 7;
+
+//Print Sparky's age in dog years.
+console.log("Sparky is" + " " + SparkyHumanAge + " " + "human years old which is" + " " + DogYears + " " + "in dog years");
