8917e1e72ad56ea8230177b4a426baca050df04f
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 09:23:30 -0400

    Slices of Pie Part 1 Expressions worksheet.

---
+++ Expressions_worksheet/js/PiePartOne.js
@@ -0,0 +1,14 @@
+// Stephanie Oldengarm, Sept. 11, 2013, Expressions Worksheet - Slice of Pie Part 1
+
+var slicesPizza = 8; //number of slices per pizza.
+
+var partyPeople = 22; //number of people at party.
+
+var pizzaOrdered = 6; //number of pizzas ordered.
+
+var totalSlices = pizzaOrdered * slicesPizza;
+
+var peopleSlices = totalSlices/partyPeople
+//number of slices per person.
+
+console.log("Each person ate" + " " + peopleSlices + " " + "slices of pizza at the party.");
