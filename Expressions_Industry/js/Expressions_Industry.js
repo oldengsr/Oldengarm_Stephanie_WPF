580d559bf68b22c0ee7310833c046b8d820781ec
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 15:38:30 -0400

    Set up an array.

--- Expressions_Industry/js/Expressions_Industry.js
+++ Expressions_Industry/js/Expressions_Industry.js
@@ -1,3 +1,15 @@
 //Stephanie Oldengarm, Sept. 11, 2013, Expressions Assignment - Industry
 
 //related to current job, managing a frozen yogurt shop
+
+var yogurtCase = 6; //6 cartons per case
+
+var yogurtUse = new Array(4); //use over one particular month
+yogurtUse[0] = a
+console.log(yogurtUse[0]);
+yogurtUse[1] = b
+console.log(yogurtUse[1]);
+yogurtUse[2] = c
+console.log(yogurtUse[2]);
+yogurtUse[3] = d
+console.log(yogurtUse[3]);
