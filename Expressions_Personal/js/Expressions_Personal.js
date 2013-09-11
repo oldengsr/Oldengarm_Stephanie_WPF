b5e518a2daafe843d19c110787071e9a3e423ddf
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 13:58:11 -0400

    Initial set up of Personal Expression - dog food.

---
+++ Expressions_Personal/js/Expressions_Personal.js
@@ -0,0 +1,3 @@
+// Stephanie Oldengarm, Sept. 11, 2013, Expressions Assignment - Personal
+
+//This is going to determine how many pounds of dog food are required per year, and how many bags you will need to purchase based on pounds, and how much you will spend on dog food per year based on chosen bag.
