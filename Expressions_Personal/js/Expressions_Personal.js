0c07e312a81fc252cf722fb0e06472e3fdf6e90e
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 13:59:28 -0400

    Added user prompts.

--- Expressions_Personal/js/Expressions_Personal.js
+++ Expressions_Personal/js/Expressions_Personal.js
@@ -1,3 +1,12 @@
 // Stephanie Oldengarm, Sept. 11, 2013, Expressions Assignment - Personal
 
 //This is going to determine how many pounds of dog food are required per year, and how many bags you will need to purchase based on pounds, and how much you will spend on dog food per year based on chosen bag.
+
+var poundsPerDay = prompt("We are calculating the pounds, number of bags, and cost of food required by your dog each year. Results will be rounded to the nearest whole number." + "\n" + "\n" + "How many pounds of food does your dog eat per day?");
+console.log(poundsPerDay); //print to console.
+
+var poundsPerBag = prompt("How many pounds are in a bag of your chosen dog food?");
+console.log(poundsPerBag); //print to console.
+
+var bagPrice = prompt("How much does this bag of chosen dog food cost?");
+console.log(bagPrice); //print to console.
