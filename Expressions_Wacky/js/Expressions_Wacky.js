27c2c79c879c267321de9f3b368dd1fe56f1fd33
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 20:11:06 -0400

    comment console.log()

--- Expressions_Wacky/js/Expressions_Wacky.js
+++ Expressions_Wacky/js/Expressions_Wacky.js
@@ -2,14 +2,14 @@
 
 //Inspired by RuPaul's Dragulator from the Logo show, Drag U, I am creating a literal calculator for a Drag Queen's weekly needs.
 
-var dragShows = prompt("Girl, how many fabulous shows do you perform each week?");
-console.log(dragShows); //A girl's got to work. Find out how many shows our performer stars in per week.
+var dragShows = prompt("Girl, how many fabulous shows do you perform each week?"); //A girl's got to work. Find out how many shows our performer stars in per week.
+console.log(dragShows); //print to console
 
-var falseLashes = prompt("Lashes are everything! How many pairs do you use to create the illusion?");
-console.log(falseLashes); //Most drag queens use multiple pairs of lashes to create their look. Find out how many our girl uses each night.
+var falseLashes = prompt("Lashes are everything! How many pairs do you use to create the illusion?"); //Most drag queens use multiple pairs of lashes to create their look. Find out how many our girl uses each night.
+console.log(falseLashes); //print to console
 
-var pantyHose = prompt("How many pantyhose do you require for your ladyboy transformation each night?");
-console.log(pantyHose); //Drag queens use multiple pairs of hosery when performing so they don't have to shave their legs! How many does ours need each night?
+var pantyHose = prompt("How many pantyhose do you require for your ladyboy transformation each night?"); //Drag queens use multiple pairs of hosery when performing so they don't have to shave their legs! How many does ours need each night?
+console.log(pantyHose); //print to console
 
 var weeklyLashes = dragShows * falseLashes
 //find out how many lashes are needed per week by multiplying the number of shows by the number of pairs used for each show.
