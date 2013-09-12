26f5356eea333a4ed7ae00ff1819a781aeffaf43
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 20:03:25 -0400

    Prompts for Drag Queen necessities.

--- Expressions_Wacky/js/Expressions_Wacky.js
+++ Expressions_Wacky/js/Expressions_Wacky.js
@@ -2,8 +2,11 @@
 
 //Inspired by RuPaul's Dragulator from the Logo show, Drag U, I am creating a literal calculator for a Drag Queen's weekly needs.
 
-var dragShows
+var dragShows = prompt("Girl, how many fabulous shows do you perform each week?");
+console.log(dragShows);
 
-var falseLashes
+var falseLashes = prompt("Lashes are everything! How many pairs do you use to create the illusion?");
+console.log(falseLashes);
 
-var pantyHose
+var pantyHose = prompt("How many pantyhose do you require for your ladyboy transformation each night?");
+console.log(pantyHose);
