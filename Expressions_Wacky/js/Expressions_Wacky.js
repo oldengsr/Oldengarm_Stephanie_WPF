682c2d0e02f313bf729dd327c4a68f9ef7b0aea9
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-11 20:02:00 -0400

    Wacky expression set up - Drag Queen calculator.

---
+++ Expressions_Wacky/js/Expressions_Wacky.js
@@ -0,0 +1,3 @@
+//Stephanie Oldengarm, Sept. 11, 2013, Expressions Assignment - Wacky
+
+//Inspired by RuPaul's Dragulator from the Logo show, Drag U, I am creating a literal calculator for a Drag Queen's weekly needs.
