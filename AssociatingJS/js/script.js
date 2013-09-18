b2424785ef5cef8c2fdd44034311072c499510c6
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-18 09:03:57 -0400

    If / Else

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,12 +1,13 @@
 // Conditional Logic - With an Expression
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 40 inches in height
-if(kidHeight + sneakerLift > 48){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+}else{
+	console.log("Sorry kid, you've got some growing to do first!");
 }
