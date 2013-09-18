efa5aeb4be9bf23797a9ed3799371aac236e9254
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-18 11:35:51 -0400

    Set up for Conditionals - Personal.

---
+++ Conditionals_Personal/Conditionals_Personal.js
@@ -0,0 +1,11 @@
+// Stephanie Oldengarm, Sept. 18, 2013, Conditionals - Personal
+
+//I like to go to the gym in the morning, but I am pressed for time if I have to go to work, and depending on how much homework I have left to do.
+
+//do I have to work?
+
+//yes - is there homework to do?
+
+//yes - no gym
+
+//no - go to the gym
---
+++ Conditionals_worksheet/js/checkTheLogin.js
@@ -0,0 +1,3 @@
+// Stephanie Oldengarm, Sept. 18, 2013, Conditionals Worksheet - Check the Login
+
+//make sure user has correct username AND password.
