b32a735414f4cece8219a7ea94bedb8ab94c49d8
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-18 09:09:16 -0400

    Else If

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -2,12 +2,17 @@
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 40 inches in height
 if(kidHeight > minHeight){
-	//code performed if condition is true
+	//you can ride
 	console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+	console.log("You can ride, but only with a parent present.");
+	//you can ride with a parent present
 }else{
-	console.log("Sorry kid, you've got some growing to do first!");
+	//sorry you have growing to do
+	console.log("Sorry kidm you've got some growing to do first!");
 }
