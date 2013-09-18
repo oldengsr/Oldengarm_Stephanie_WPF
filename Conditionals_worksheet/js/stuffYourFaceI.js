58df76710d2ef49782921e185ef6d42052fa0d96
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-18 09:56:30 -0400

    Stuff Your Face I

---
+++ Conditionals_worksheet/js/stuffYourFaceI.js
@@ -0,0 +1,12 @@
+// Stephanie Oldengarm, September 18, 2013, Conditionals Worksheet - Stuff Your Face I
+
+//You must weigh 250 lbs. or more to be in heavyweight division of pie eating contest
+
+compWeight = 245;
+minWeight = 250;
+
+if(compWeight >= 250){
+	console.log("The competitor qualifies for the heavyweight division.");
+}else{
+	console.log("The competitor needs to gain some weight!");
+}
