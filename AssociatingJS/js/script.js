996c84e0f2bc620e1180570759f2baeeefe31f53
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-18 09:00:00 -0400

    Conditional Expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,11 +1,12 @@
-// Relational Expressions
+// Conditional Logic - With an Expression
 
-var kidHeight = 40;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 40 inches in height
-if(kidHeight > 48){
+if(kidHeight + sneakerLift > 48){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
