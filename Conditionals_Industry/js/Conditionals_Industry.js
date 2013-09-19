8f637a16e6aa7d762fd9fe76f436539ed914310b
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:14:56 -0400

    Set up problem in words.

--- Conditionals_Industry/js/Conditionals_Industry.js
+++ Conditionals_Industry/js/Conditionals_Industry.js
@@ -1,1 +1,21 @@
 // Stephanie Oldengarm, Sept. 18, 2013, Conditionals - Industry
+
+//Frequent Diner program allows for a free small yogurt every time you reach $50 spent on your Frequent Diner account.
+
+//you need a minimum of 50 points to recieve a free small
+
+
+//prompt #1 collect user information
+
+//prompt #2 collect user information
+//two prompts to collect user information
+
+//tell user how many points they have left over.
+
+//if you have 50 points or more
+
+//if the freqDinerCredit is greater than or equal to freeYogurtMin, the user will recieve a free small yogurt. //relational operator
+
+//Let user know how many points they have after earning their free small yogurt.
+
+//if user has not reached 50 points, they will recieve a notification that they aren't there yet.
