39b7b2690988e9cc486e4e75e24a5632f0f18a25
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:18:52 -0400

    Set up problem in words.

--- Conditionals_Personal/js/Conditionals_Personal.js
+++ Conditionals_Personal/js/Conditionals_Personal.js
@@ -1,1 +1,11 @@
 // Stephanie Oldengarm, Sept. 18, 2013, Conditionals - Personal
+
+//How quickly can I catch up on my favorite TV show?
+
+//user prompt #1 how many episodes you have left.
+
+//user prompt #2 how many minutes are in each episode.
+
+//user prompt #3 how many minutes you want to spend on your show each week.
+
+//calculation of total time (in minutes) required to finish your favorite TV show.
