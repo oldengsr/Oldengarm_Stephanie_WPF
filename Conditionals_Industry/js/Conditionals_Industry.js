d3c6cf1d78c88a03d3f7794a170389ebc3690190
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:25:12 -0400

    Increase spacing.

--- Conditionals_Industry/js/Conditionals_Industry.js
+++ Conditionals_Industry/js/Conditionals_Industry.js
@@ -4,11 +4,14 @@
 
 var freeYogurtMin = 50;
 //you need a minimum of 50 points to recieve a free small
+
 var userName = prompt("To access your Frequent Diner information, please enter your first and last name:");
 //prompt #1 collect user information
+
 var freqDinerCredit = prompt("How many Frequent Diner Points do you have?");
 //prompt #2 collect user information
 //two prompts to collect user information
+
 var leftOverPoints = freqDinerCredit - freeYogurtMin;
 //tell user how many points they have left over.
 
