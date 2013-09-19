58b46b65ba2f8a289fd6e00b589e897d92b97eef
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:17:10 -0400

    Set up problem in words.

--- Conditionals_Wacky/js/Conditionals_Wacky.js
+++ Conditionals_Wacky/js/Conditionals_Wacky.js
@@ -1,1 +1,15 @@
 //Stephanie Oldengarm, Sept. 18, 2013, Conditionals - Wacky
+
+//I recently finished reading The Hunger Games trilogy by Suzanne Collins, and wanted to use a Victory Odds as my Wacky Conditional example.
+
+//Prompt for District number
+
+//Tributes with a special talent (archery, sword throwing, etc.) have a higher chance of winning tham those without.
+
+//The Districts 1, 2, and 4 usually stand a higher chance of winning the Games, and are called Careers, because they often enjoy and train specifically to be Tributes.
+
+//If you are from 1, 2, or 4, and possess a special talent, your odds of winning are very high.
+
+//If you are from 1, 2 or 4, and do not possess a special talent, while it can make the Games harder, you are still in a position to win.
+
+//District 12 is at a disadvantage to the other Districts.
