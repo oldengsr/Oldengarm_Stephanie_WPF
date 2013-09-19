1091c066af0e7479f763b3cb651f208d9e6bcdb3
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:21:31 -0400

    Update comments.

--- Conditionals_Personal/js/Conditionals_Personal.js
+++ Conditionals_Personal/js/Conditionals_Personal.js
@@ -9,7 +9,7 @@ var episodeTime = prompt("How many minutes are in each episode?");
 var timeDedicated = prompt("How much time (in minutes) are you willing to dedicate towards watching your show per week?");
 //user prompt #3 how many minutes you want to spend on your show each week.
 var totalTime = totalEpisodes * episodeTime;
-//calculation of total time (in minutes) required to finish your favorite TV show.
+//take the episodes left and multiply them by the minutes in each episode to arrive at the total time needed to finish the show.
 
 (timeDedicated >= totalTime) ? console.log("By dedicating " + timeDedicated + " minutes per week, with " + totalTime + " minutes to watch, you can catch up within a week!") : console.log("It will take you more than a week to catch up on your show. Try increasing the minutes you are willing to dedicate. You need to catch up on " + totalTime + " minutes total.")
 //ternary - (condition) ? result1 : result2
