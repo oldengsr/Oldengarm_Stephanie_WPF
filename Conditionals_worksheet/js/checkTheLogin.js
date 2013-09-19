714c9550693946355657484390edc5dbb2e03148
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 10:16:13 -0400

    Check the Login exercise.

--- Conditionals_worksheet/js/checkTheLogin.js
+++ Conditionals_worksheet/js/checkTheLogin.js
@@ -1,3 +1,17 @@
 // Stephanie Oldengarm, Sept. 18, 2013, Conditionals Worksheet - Check the Login
 
 //make sure user has correct username AND password.
+
+var userName = prompt("Please enter your username:");
+var password = prompt("Please enter your password:");
+
+var correctName = "user";
+var correctPass = "password";
+
+if(userName == correctName && password == correctPass){
+	console.log("Welcome, " + userName + "!")
+}else if(userName != correctName && password == correctPass){
+	console.log("User name does not match our records.")
+}else{
+	console.log("Password does not match our records.")
+}
