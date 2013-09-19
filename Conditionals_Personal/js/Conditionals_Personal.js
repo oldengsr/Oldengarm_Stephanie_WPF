93d77dc9d2a1ce19f3e452afd991d494c20f5353
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-19 09:12:57 -0400

    Initial set up.

--- Conditionals_Personal/js/Conditionals_Personal.js
+++ Conditionals_Personal/js/Conditionals_Personal.js
@@ -1,16 +1,1 @@
 // Stephanie Oldengarm, Sept. 18, 2013, Conditionals - Personal
-
-//I like to go to the gym in the morning, but I am pressed for time if I have to go to work, and depending on how much homework I have left to do.
-
-var workDay = true; //do I have to work?
-var homeWork = false; //do I have homework?
-
-
-if(workDay = true){
-	console.log(prompt("You have work today! Do you have homework to do?"));
-}else if(homeWork = false){
-	console.log("No homework? You do have time for the gym this morning!");
-	//
-}else{
-	console.log("Sorry, no time for the gym this morning.")
-}//you have work and homework, no time for gym in the morning
