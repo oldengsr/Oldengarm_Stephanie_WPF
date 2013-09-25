b7450d7d2a724f902c27e055830af692fcb211b3
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-25 09:28:36 -0400

    Functions vs Procedures

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,13 @@
-// Functions - Returning Values
+// Functions - Function vs Procedure
 
-var total = calcArea(30, 20); //invoking function here
-
-function calcArea(w, h){
-	var area = w * h;
-	return area; //function spitting the info out
+//This is a function
+function calcAreaF(width, height){
+	var area = width * height;
+	return area;
 }
-console.log(total);
+
+//This is a procedure
+function calcAreaP(width, height){
+	var area = width * height;
+	console.log(area); //no return
+}
