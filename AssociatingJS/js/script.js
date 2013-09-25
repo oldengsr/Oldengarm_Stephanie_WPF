7e550a4c49a36ab8a39fd5d2be6670fe2f39e696
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-25 09:34:12 -0400

    Anonymous Functions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,13 +1,14 @@
-// Functions - Function vs Procedure
+// Functions - Anonymous Functions
 
-//This is a function
-function calcAreaF(width, height){
+//also called closures
+//are functions created as the code is run
+//must be declared before they are called
+
+var calcArea = function(width, height){ //defining
 	var area = width * height;
 	return area;
 }
 
-//This is a procedure
-function calcAreaP(width, height){
-	var area = width * height;
-	console.log(area); //no return
-}
+var a = calcArea(20, 30) //invoking
+
+console.log(a);
