304c72bb1d73ce6b82f55ba41de02490f9172aca
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:24:17 -0400

    prompt

--- Functions_Personal/js/Functions_Personal.js
+++ Functions_Personal/js/Functions_Personal.js
@@ -1,3 +1,6 @@
 // Stephanie Oldengarm, Sept. 25, 2013, Functions Assignment - Personal
 
 //Most vehicle manufacturers recommend changing the oil in your car ever 3000 miles.
+
+var carMileage = Number(prompt("How many miles have you driven since your last oil change?"));
+// prompt user for info
