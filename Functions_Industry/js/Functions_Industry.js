fc0c903558cea0467d59eeacfd57f817c9465516
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:22:36 -0400

    function

--- Functions_Industry/js/Functions_Industry.js
+++ Functions_Industry/js/Functions_Industry.js
@@ -6,3 +6,7 @@ var ouncesWant = prompt("How many ounces of yogurt do you want?"); //prompt
 var calsPerOunce = prompt("How many calories per ounce?"); //prompt
 var calLimit = prompt("What is your calorie limit for your yogurt?"); //prompt
 
+function calsPerCup(cupSize, calCup, limitCal){ //function and parameters
+	var totalCals = (cupSize * calCup) <= limitCal;
+	return totalCals; //return statement
+}
