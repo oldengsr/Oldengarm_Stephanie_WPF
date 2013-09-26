462520f7838397da08933553a7c34681a942f6a6
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:26:12 -0400

    Set up Wacky Functions

---
+++ Functions_Wacky/js/Functions_Wacky.js
@@ -0,0 +1,3 @@
+// Stephanie Oldengarm, Sept. 25, 2013, Functions Assignment - Wacky
+
+//Determine vampire feeding skills. As long as the vampire requires less than 30% of human blood supply, they are safe.
