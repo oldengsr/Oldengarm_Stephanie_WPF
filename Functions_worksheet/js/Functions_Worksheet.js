fa412b3f32205d550ddd96246a16e13726be6b78
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:37:05 -0400

    Functions Worksheet finished

--- Functions_worksheet/js/Functions_Worksheet.js
+++ Functions_worksheet/js/Functions_Worksheet.js
@@ -1,33 +1,35 @@
 // Stephanie Oldengarm, Sept. 25, 2013, Functions Worksheet
 
-
 //CIRCUMFERENCE
 //Calculate the circumference of a circle.
 
+var radius = prompt("Please enter the radius of the circle."); //user prompt
 var pi = 3.14;
-var radius = prompt("Please enter the radius of the circle."); //parameters - radius
 var diameter = radius * 2;
-var circumference = calcCircum(diameter, pi);
+
+function calcCircum(diameter, pi){ //parameters
+	var circum = diameter * pi;
+	return circum; //return
+}
+
+var circumference = calcCircum(diameter, pi); //argument
 
 console.log("The circumference of the circle is " + circumference);
 //print to console "The circumference of the circle is X"
 
-function calcCircum(diameter, pi)
-{
-	return diameter * pi //return - circumference
-}
-
 //STUNG!
 //Calculate how many bee stings are needed to kill an animal.
 
-var victimWeight = prompt("Please enter the weight of the animal.");
+var victimWeight = prompt("Please enter the weight of the animal."); //user prompt
 //parameters - victim weight in pounds
-var deadlyBeeStings = 8.666666667 * victimWeight;
+var deadlyBeeStings = 8.666666667;
 
-console.log("It takes " + deadlyBeeStings + " bee stings to kill this animal.");
-//print to console "It takes X bee stings to kill this animal"
-
-function deadlyStingsFunction(deadlyBeeStings)
-{
-	return deadlyBeeStings //return - no. bee stings
+function deadlyStingsFunction(stings, weight){ //parameters
+	var ouch = stings * weight;
+	return ouch; //return
 }
+
+var dead = deadlyStingsFunction(deadlyBeeStings, victimWeight); //argument
+
+console.log("It takes " + dead + " bee stings to kill this animal.");
+//print to console "It takes X bee stings to kill this animal"
