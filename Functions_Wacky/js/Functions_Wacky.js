e5d2ec7894e5180b6245db233062469cb3cc501e
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:26:33 -0400

    variable 1

--- Functions_Wacky/js/Functions_Wacky.js
+++ Functions_Wacky/js/Functions_Wacky.js
@@ -1,3 +1,5 @@
 // Stephanie Oldengarm, Sept. 25, 2013, Functions Assignment - Wacky
 
 //Determine vampire feeding skills. As long as the vampire requires less than 30% of human blood supply, they are safe.
+
+var bloodTotal = 5; //Adult human contains approx. 5 litres of blood
