6fde5215029786a487d7cba93180c6d28c3423be
Stephanie Oldengarm <oldengsr@gmail.com>
2013-09-26 10:21:44 -0400

    prompt 1

--- Functions_Industry/js/Functions_Industry.js
+++ Functions_Industry/js/Functions_Industry.js
@@ -1,3 +1,5 @@
 // Stephanie Oldengarm, Sept. 25, 2013, Functions Assignment - Industry
 
 //Can you meet your calorie requirements and enjoy a cup of frozen yogurt?
+
+var ouncesWant = prompt("How many ounces of yogurt do you want?"); //prompt
